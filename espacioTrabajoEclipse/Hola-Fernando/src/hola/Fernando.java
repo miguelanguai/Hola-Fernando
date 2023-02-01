ea21dee1523704a096fd41a9c7b868dd6694e661
Pablo Cru <122596837+pabcrudel@users.noreply.github.com>
2023-02-01 12:30:03 +0100

    Quito una a

--- espacioTrabajoEclipse/Hola-Fernando/src/hola/Fernando.java
+++ espacioTrabajoEclipse/Hola-Fernando/src/hola/Fernando.java
@@ -4,7 +4,7 @@ public class Fernando {
 
 	public static void main(String[] args) {
 		System.out.println("El Nano ah-eh, el Nano ah-oh");
-		//Esperando el mensaje de Fernando Garfella
+		//Esperando el mensaje de Fernando Garfell
 		System.out.println("No quiero a barrichello, Schumacher ni al button");
 		
 	}
