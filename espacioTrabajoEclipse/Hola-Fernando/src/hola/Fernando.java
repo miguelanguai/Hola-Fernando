9099127267869c4a97122afc11f26651da48d56c
Miguel Angel <migguavil@alu.edu.gva.es>
2023-02-01 11:26:19 +0100

    Primer commit

---
+++ espacioTrabajoEclipse/Hola-Fernando/src/hola/Fernando.java
@@ -0,0 +1,11 @@
+package hola;
+
+public class Fernando {
+
+	public static void main(String[] args) {
+		System.out.println("El Nano ah-eh, el Nano ah-oh");
+		//Esperando el mensaje de Fernando Garfella
+		
+	}
+
+}
